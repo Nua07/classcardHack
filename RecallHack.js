67a5ca135d8c00ae057999dede16b5c9c4da1840
Nua07 <59082853+Nua07@users.noreply.github.com>
2020-08-20 09:33:55 +0900

    Create RecallHack.js

---
+++ RecallHack.js
@@ -0,0 +1,10 @@
+//리콜 내에서 실행
+arr_send_card_idx=[0]
+arr_send_score=[0]
+
+for(var i=0;i<study_data.length;i++){
+    arr_send_card_idx.push(study_data[i].card_idx)
+    arr_send_score.push(1)
+}
+
+sendLearnAll()
