238b0223b0eadfedbf8df907159defbbe3d830d3
nua_kr <59082853+Nua07@users.noreply.github.com>
2021-04-16 08:48:04 +0900

    Create TestHack.js

---
+++ TestHack.js
@@ -0,0 +1,17 @@
+let cards = $("div.flip-card")
+
+for(let i = 0; i<cards.length; i++) {
+    let tq = $("input[name='test_question[]']", cards[i]).val()
+    let ans = obj_answer["q" + tq]
+    let input_wrap = $("div.cc-radio-box", cards[i])
+    
+    for(let j = 0; j<input_wrap.length; j++) {
+        let txt = $("label>div>div", input_wrap[j]).text()
+        let inp = $("input", input_wrap[j])
+        
+        if(txt === ans) {
+            $("input.user_answer", cards[i]).val(inp.val())
+            break
+        }
+    }
+}
